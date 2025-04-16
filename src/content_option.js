8e61ba0dfa88fdb7e5a2671a2c570b11dae30611
sonaojus <sonaojus.job@gmail.com>
2025-04-16 15:00:08 -0400

    The initial structure of the portfolio template with Email JS integration

---
+++ src/content_option.js
@@ -0,0 +1,179 @@
+const logotext = "SONA";
+const meta = {
+    title: "Sona Ojus",
+    description: "I’m Sona Ojus. If web development is art, then I am an artist with a keyboard.",
+};
+
+const introdata = {
+    title: "I’m Sona Ojus",
+    animated: {
+        first: "I turn coffee into clean code and interactive experiences",
+        second: "I’m all about creating digital magic that works and wows.",
+        third: "Let's paint pixels with React and sculpt some logic with Node.js"
+    },
+    description: `Full-stack developer with a serious love for crafting elegant, efficient, and impactful web applications. 
+    I’m passionate about every layer of the stack`,
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "A bit about myself",
+    aboutme: `Result-driven and dedicated Lead Software Developer with over 8 years of extensive IT experience spanning Ecommerce, 
+    Industrial Automation, and Retail domains. Proven expertise in designing and delivering robust, 
+    scalable solutions with a focus on Frontend and MEAN stack development. Proficient in modern technologies including 
+    React, TypeScript, Python, JavaScript, Redux, Next.js, Node.js, Express.js, WebSockets, HTML5, SASS/LESS, gRPC, MongoDB and GraphQL. 
+    Adept at leading cross-functional teams, driving innovation, and optimizing development workflows to achieve business goals efficiently. 
+    Possesses strong expertise in Data structures and algorithms, 
+    advanced programming across multiple languages and comprehensive knowledge on Enterprise system design.`
+};
+const worktimeline = [{
+    jobtitle: "Software Developer (Co-op)",
+    where: "Pernod Ricard",
+    date: "Sep 2024 - Dec 2025",
+},
+{
+    jobtitle: "Senior Software Engineer III",
+    where: "Rakuten",
+    date: "Sep 2020 - Aug 2023",
+},
+{
+    jobtitle: "Senior Software Engineer",
+    where: "Toshiba",
+    date: "Oct 2018 - Sep 2020",
+},
+{
+    jobtitle: "Software Developer",
+    where: "High Peak Software",
+    date: "Oct 2015 - Sep 2018",
+},
+];
+
+const skills = [{
+    name: "React JS",
+    value: 99,
+},
+{
+    name: "Next JS",
+    value: 90,
+},
+{
+    name: "HTML/CSS/Javascript",
+    value: 95,
+},
+{
+    name: "Node JS",
+    value: 85,
+},
+{
+    name: "Python",
+    value: 60,
+}
+];
+
+const services = [{
+    title: "Custom Front-End Development",
+    description: "Build dynamic, responsive web applications using React, Next.js, and TypeScript. Component-based architecture, Hooks, Redux, and SSR (Server Side Rendering) with SEO optimization.",
+},
+{
+    title: "E-commerce Web Solutions",
+    description: "Build and optimize online storefronts with shopping cart systems, affiliate integration, and user dashboards. Experience building Rakuten Kobo’s high-traffic apps? That’s a flex.",
+},
+{
+    title: "Micro Frontend Architecture Setup",
+    description: "Modular web architecture for large-scale enterprise apps with independent deployment and better maintainability.",
+},
+];
+
+const dataportfolio = [{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/800/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/600/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/300/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/700/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+
+{
+    img: "https://picsum.photos/400/600/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/300/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/550/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/700/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "sonaojus.job@gmail.com",
+    YOUR_FONE: "(437)838-0481",
+    description: `Currently based out of Kitchener, Ontario, I’m your go-to web developer if you’re looking to level up your business with a powerful online presence. 
+    Whether you’re a startup needing your first website or an established brand ready to scale, I am here to help you succeed.`,
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_qhlc24o",
+    YOUR_TEMPLATE_ID: "template_1s931ft",
+    YOUR_USER_ID: "4PvnBn1kjFy_luiyB",
+};
+
+const socialprofils = {
+    github: "https://github.com/sonaojus93",
+    facebook: "https://www.facebook.com/sona.ojus",
+    linkedin: "https://www.linkedin.com/in/sona-ojus/",
+    twitter: "https://x.com/ojus_sona",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
